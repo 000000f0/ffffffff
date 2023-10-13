aa83d55e8a3fe288ad93b471d08ab04bcedd9a16
000000f0 <131445668+000000f0@users.noreply.github.com>
2023-10-13 13:23:38 +0200

    Add files via upload

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    '/api', // Define the API endpoint to be proxied
     createProxyMiddleware({
-      target: 'https://deva4.fly.dev',
-      changeOrigin: true,
+      target: 'https://deva4.fly.dev', // The target URL to proxy requests to
+      changeOrigin: true, // Change the origin of the host header to the target URL
     })
   );
 };
